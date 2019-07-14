eb312d91ac4b1ca6e6cbd453b75033dba95f566f
Administrator <2962719743@qq.com>
2019-07-14 22:56:26 +0800

    项目可正常通过User的name查询，并且返回字符串

--- src/main/java/cjl/zfend/controller/LoginController.java
+++ src/main/java/cjl/zfend/controller/LoginController.java
@@ -1,4 +1,20 @@
 package cjl.zfend.controller;
 
+import cjl.zfend.dao.UserDao;
+import cjl.zfend.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api")
 public class LoginController {
+    @Autowired
+    private UserDao userDao;
+    @RequestMapping("/login")
+    public User login(@RequestParam String name){
+      User user1= userDao.selectUser(name);
+    return  user1;
+    }
 }
--- src/main/java/cjl/zfend/service/UserService.java
+++ src/main/java/cjl/zfend/service/UserService.java
@@ -8,5 +8,4 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserDao userDao;
-
 }
